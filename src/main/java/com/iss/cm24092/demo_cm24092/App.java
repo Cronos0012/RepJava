579ed1bbd46538f8a10630c4a0ccd5bc4ddc77ed
Cronos0012 <brandonaxel0012@gmail.com>
2024-05-05 00:35:12 -0600

    commit

---
+++ src/main/java/com/iss/cm24092/demo_cm24092/App.java
@@ -0,0 +1,19 @@
+package com.iss.cm24092.demo_cm24092;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello world";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( "Hello World!" );
+    }
+    
+}
---
+++ src/test/java/com/iss/cm24092/demo_cm24092/AppTest.java
@@ -0,0 +1,21 @@
+package com.iss.cm24092.demo_cm24092;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testhello()
+    {
+        assertTrue( true );
+    }
+}
